c7eaef089ebb025d72b8ace5715ac5e7d9cf9552
sksayril <sksayril123@gmail.com>
2026-02-20 22:51:46 +0530

    fixed the issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.superloan.app"
+    namespace = "com.aryatechsuperloan"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.superloan.app"
+        applicationId = "com.aryatechsuperloan"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/aryatechsuperloan/MainActivity.kt
+++ android/app/src/main/kotlin/com/aryatechsuperloan/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.superloan.app
+package com.aryatechsuperloan
 
 import io.flutter.embedding.android.FlutterActivity
 
